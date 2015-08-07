9c8af07103a5150a68b1f2c6f08d30b76e464335
Wataru Inaba <gitforwi@gmail.com>
2015-08-08 02:12:49 +0900

    20150807

--- src/servlet/Main.java
+++ src/servlet/Main.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import model.Mutter;
+import model.PostMutterLogic;
 import model.User;
 
 /**
@@ -61,5 +62,39 @@ public class Main extends HttpServlet {
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
 			dispatcher.forward(request, response);
 		}
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response)
+			throws ServletException, IOException{
+		// リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+
+		// 入力値のチェック
+		if(text != null && text.length() != 0) {
+			// アプリケーションスコープに保存されたつぶやきリストを取得
+			ServletContext application = this.getServletContext();
+			List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
+
+			//セッションスコープに保存されたユーザ情報を取得
+			HttpSession session = request.getSession();
+			User loginUser = (User) session.getAttribute("loginUser");
+
+			//つぶやきリストに追加
+			Mutter mutter = new Mutter(loginUser.getName(), text);
+			PostMutterLogic postMutterLogic = new PostMutterLogic();
+			postMutterLogic.execute(mutter, mutterList);
+
+			// アプリケーションスコープにつぶやきリストを保存
+			application.setAttribute("mutterList", mutterList);
+		} else {
+			String errMsg = "つぶやきが入力されていません";
+			request.setAttribute("ErrMsg", errMsg);
+		}
+		// メイン画面にフォワード
+		RequestDispatcher dispatcher =
+				request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+		dispatcher.forward(request, response);
 	}
 }
